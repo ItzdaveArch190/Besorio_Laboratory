a8e97339a37745eab26f0e2f4fa7037835f22c2c
ItzdaveArch190 <tlga.davebesorio@gmail.com>
2025-04-07 16:36:37 +0800

    add Lab3

--- Lab3_Inheritance.java
+++ Lab3_Inheritance.java
@@ -1,3 +1,6 @@
 public class Lab3_Inheritance {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Create a new instance of the Student class
+    }
 }
